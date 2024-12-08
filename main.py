6b43b357d6c99f139188236a476cab0f670a9ef5
Mason-Dino <mason.n.dinosaur@gmail.com>
2024-12-08 11:12:28 -0600

    starting ssh connection

---
+++ main.py
@@ -0,0 +1,8 @@
+import paramiko
+import os
+
+def ssh_connect(hostname, username, password):
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(hostname=hostname, username=username, password=password)
+    return ssh_client
