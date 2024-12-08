e2ce4c28a15076b4b97fb35ee5ff502fa3a24fcc
Mason-Dino <mason.n.dinosaur@gmail.com>
2024-12-08 11:13:28 -0600

    working on connection

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import paramiko
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 def ssh_connect(hostname, username, password):
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh_client.connect(hostname=hostname, username=username, password=password)
     return ssh_client
+
+def main():
+    hostname = os.getenv('HOSTNAME')
+    username = os.getenv('USERNAME')
+    password = os.getenv('PASSWORD')
+    
+    ssh_client = ssh_connect(hostname, username, password)
+    print("Connected to {} as {}".format(hostname, username))
+    
+    ssh_client.close()
+
+if __name__ == "__main__":
+    main()
